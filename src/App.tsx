4efcae0e0bbf46dce767565bb97d8fe79c7065ef
Anderson Casimiro <cassgkn@hotmail.com>
2024-01-09 11:00:50 -0300

    atualizado a porta do servidor de localhost: 4000 para localhost: 5000

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ export type Game = {
 }
 
 function App() {
+  console.log('redenrizando')
   return (
     <Provider store={store}>
       <GlobalStyle />
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,9 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-
 import { Game } from '../App'
 
 const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:4000'
+    baseUrl: 'http://localhost:5000'
   }),
   endpoints: (builder) => ({
     getJogos: builder.query<Game[], void>({
